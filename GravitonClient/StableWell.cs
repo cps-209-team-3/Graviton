bfeced300e99a059236c4ec54547fac0a9adbb79
eross156 <eross156@students.bju.edu>
2019-03-28 18:22:58 -0400

    modified class diagram

---
+++ GravitonClient/StableWell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GravitonClient
+{
+    class StableWell : Well
+    {
+        int ticksLeft;
+        public int orbs;
+        public StableWell()
+        {
+
+        }
+
+        public override string Serialize()
+        {
+            return null;
+        }
+    }
+}
