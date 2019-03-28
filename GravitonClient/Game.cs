b0f7207132a8e8da0cddf9e8c799abe246503c7c
eross156 <eross156@students.bju.edu>
2019-03-28 13:40:20 -0400

    added class diagram and structure

---
+++ GravitonClient/Game.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+
+namespace GravitonClient
+{
+    class Game
+    {
+        public event EventHandler<int> BoardUpdatedEvent;
+        public int wasdDepressed;
+        public DispatcherTimer timer;
+        public List<Well> wells;
+        public Ship user;
+        public List<Orb> orbs;
+        public Game()
+        {
+
+        }
+
+
+        public void Load(string filename)
+        {
+            
+        }
+
+        public void Save(string filename)
+        {
+
+        }
+        public void WASDPressed(int key)
+        {
+
+        }
+        public void Timer_Tick()
+        {
+
+        }
+        public void UpdateWells()
+        {
+
+        }
+        public void UpdateUser()
+        {
+
+        }
+        public void OnOrb()
+        {
+
+        }
+        public void OnWell()
+        {
+
+        }
+
+    }
+}
---
+++ GravitonClient/Ship.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GravitonClient
+{
+    class Ship
+    {
+        public double xcoor;
+        public double ycoor;
+        public Orb[] orbs;
+        public Ship(double xcoor, double ycoor)
+        {
+
+        }
+
+        public string Serialize()
+        {
+            return null;
+        }
+
+        public static Ship Deserialize(string info)
+        {
+            return null;
+        }
+    }
+}
