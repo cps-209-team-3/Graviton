b2b10050ba66c2261b27f7c45ed81ce38c1dba71
Stamine6 <44934335+Stamine6@users.noreply.github.com>
2019-03-31 20:13:45 -0400

    Basic class structure finished

--- GravitonClient/HighScoresTest.cs
+++ GravitonClient/HighScoresTest.cs
@@ -4,3 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+
+namespace GravitonClient
+{
+    [TestFixture]
+    public class HighScoresTest
+    {
+      
+    }
+}
