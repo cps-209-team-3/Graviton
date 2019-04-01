9a1b3ef15bd5ee2fedb48aa7c93b92d9c0645ec8
Stamine6 <44934335+Stamine6@users.noreply.github.com>
2019-03-31 20:04:11 -0400

    Update HighScores.cs

--- GravitonClient/HighScores.cs
+++ GravitonClient/HighScores.cs
@@ -15,22 +15,55 @@ namespace GravitonClient
             set{hiScores = value;}
         }
         
+        public HighScores(Dictionary<string, int> scoreDict)
+        {
+            
+        }
+        
+        // Checks a game to see if any scores should be included in the list of highscores.
+        // Accepts a reference to a Game instance.
+        // Returns nothing.
         public void CheckNewScores(Game game)
         {
             
         }
         
+        // Adds a new score to the high score list (also deletes lowest one).
+        // Accepts a username as a string and a score as an integer.
+        // Returns nothing.
         public void AddNewScore(string username, int score)
         {
             
         }
         
-        public void Serialize()
+        // Creates a HighScores object from a file.
+        // Accepts a path to the file as a string.
+        // Returns a HighScores object.
+        public static HighScores Load(string path)
         {
             
         }
         
-        public void Deserialize()
+        // Writes the current HighScores object to a file.
+        // Accepts a path to the file as a string.
+        // Returns nothing.
+        public void Save(string path)
+        {
+            
+        }
+        
+        // Serializes the HighScores object to string.
+        // Accepts nothing.
+        // Returns a string.
+        public string Serialize()
+        {
+            
+        }
+        
+        // Deserializes a HighScores object from a string.
+        // Accepts a serialized object as a string.
+        // Returns a HighScores object.
+        public static HighScores Deserialize(string serialized)
         {
             
         }
