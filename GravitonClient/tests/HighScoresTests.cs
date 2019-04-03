6e3b2f0d4f6c58cbbab272710ca271a84221ff97
Stamine6 <44934335+Stamine6@users.noreply.github.com>
2019-04-03 00:48:01 -0400

    added tests back

--- GravitonClient/tests/HighScoresTests.cs
+++ GravitonClient/tests/HighScoresTests.cs
@@ -74,11 +74,96 @@ namespace GravitonClient
             Assert.IsTrue(hiScore.HiScores.Contains(new HiScore("Evisserate", 10)));
         }
 
-       
-       
 
-        
+        [Test]
+        public void Load_InputFile_ReturnsHighScoresObject()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "testHSLoadFile.json");
+            
+            HighScores hiScore = new HighScores(new List<HiScore>());
+            hiScore.HiScores.Add(new HiScore("ben", 123));
+            hiScore.HiScores.Add(new HiScore("act", 234));
+            hiScore.HiScores.Add(new HiScore("deb", 345));
+            hiScore.HiScores.Add(new HiScore("hess", 456));
+            hiScore.HiScores.Add(new HiScore("dark", 567));
+            hiScore.HiScores.Add(new HiScore("bluff", 678));
+            hiScore.HiScores.Add(new HiScore("schaub", 789));
+            hiScore.HiScores.Add(new HiScore("mcgee", 890));
+            hiScore.HiScores.Add(new HiScore("knisely", 903));
+            hiScore.HiScores.Add(new HiScore("watson", 1));
+            
+            HighScores score = HighScores.Load(path);
+            Assert.IsTrue(score == hiScore);
+        }
 
-       
+        [Test]
+        public void Save_InputFile_NoExceptionThrown()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "testHSSaveFile.json");
+            
+            HighScores hiScore = new HighScores(new List<HiScore>());
+            hiScore.HiScores.Add(new HiScore("ben", 123));
+            hiScore.HiScores.Add(new HiScore("act", 234));
+            hiScore.HiScores.Add(new HiScore("deb", 345));
+            hiScore.HiScores.Add(new HiScore("hess", 456));
+            hiScore.HiScores.Add(new HiScore("dark", 567));
+            hiScore.HiScores.Add(new HiScore("bluff", 678));
+            hiScore.HiScores.Add(new HiScore("schaub", 789));
+            hiScore.HiScores.Add(new HiScore("mcgee", 890));
+            hiScore.HiScores.Add(new HiScore("knisely", 903));
+            hiScore.HiScores.Add(new HiScore("watson", 1));
+            try
+            {
+                hiScore.Save(path);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail();
+            }
+            
+            HighScores score = HighScores.Load(path);
+            Assert.IsTrue(score == hiScore);
+        }
+
+        [Test]
+        public void Serialize_InitializedObject_StringNotEmpty()
+        {
+            string testval = ""; //TODO
+            HighScores hiScore = new HighScores(new List<HiScore>());
+            hiScore.HiScores.Add(new HiScore("ben", 123));
+            hiScore.HiScores.Add(new HiScore("act", 234));
+            hiScore.HiScores.Add(new HiScore("deb", 345));
+            hiScore.HiScores.Add(new HiScore("hess", 456));
+            hiScore.HiScores.Add(new HiScore("dark", 567));
+            hiScore.HiScores.Add(new HiScore("bluff", 678));
+            hiScore.HiScores.Add(new HiScore("schaub", 789));
+            hiScore.HiScores.Add(new HiScore("mcgee", 890));
+            hiScore.HiScores.Add(new HiScore("knisely", 903));
+            hiScore.HiScores.Add(new HiScore("watson", 1));
+            
+            string serialized = hiScore.Serialize();
+            Assert.IsTrue(serialized == testval);
+        }
+
+        [Test]
+        public void Deserialize_InputSerializedString_ReturnsHighScoresObject()
+        {
+            string serialized = ""; //TODO
+            
+            HighScores hiScore = new HighScores(new List<HiScore>());
+            hiScore.HiScores.Add(new HiScore("ben", 123));
+            hiScore.HiScores.Add(new HiScore("act", 234));
+            hiScore.HiScores.Add(new HiScore("deb", 345));
+            hiScore.HiScores.Add(new HiScore("hess", 456));
+            hiScore.HiScores.Add(new HiScore("dark", 567));
+            hiScore.HiScores.Add(new HiScore("bluff", 678));
+            hiScore.HiScores.Add(new HiScore("schaub", 789));
+            hiScore.HiScores.Add(new HiScore("mcgee", 890));
+            hiScore.HiScores.Add(new HiScore("knisely", 903));
+            hiScore.HiScores.Add(new HiScore("watson", 1));
+            
+            HighScores score = HighScores.Deserialize(serialized);
+            Assert.IsTrue(score == hiScore);
+        }
     }
 }
