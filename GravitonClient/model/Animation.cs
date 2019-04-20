7bc4bc87167fc3ba3477e483c8fa1b3e299ea43f
bviss445 <bviss445@students.bju.edu>
2019-04-20 16:23:31 -0400

    Committing animation changes.

---
+++ GravitonClient/model/Animation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace GravitonClient.model
+{
+    class Animation
+    {
+        private BitmapImage[] imageList;
+        public BitmapImage[] ImageList
+        {
+            get { return imageList; }
+            set { imageList = value; }
+        }
+
+        private int[] tickDiffs;
+        public int[] TickDiffs
+        {
+            get { return tickDiffs; }
+            set { tickDiffs = value; }
+        }
+
+        private BitmapImage currentImage;
+        public BitmapImage CurrentImage
+        {
+            get { return currentImage; }
+            set { currentImage = value; }
+        }
+
+        private int maxTicks;
+        public int MaxTicks
+        {
+            get { return maxTicks; }
+            set { maxTicks = value; }
+        }
+
+        private int passedTicks;
+        public int PassedTicks
+        {
+            get { return passedTicks; }
+            set { passedTicks = value; }
+        }
+    }
+}
---
+++ GravitonClient/view/Animator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace GravitonClient.model
+{
+    class Animator
+    {
+        private Image displayImage;
+        public Image DisplayImage
+        {
+            get { return displayImage; }
+            set { displayImage = value; }
+        }
+
+        private Canvas displayCanvas;
+        public Canvas DisplayCanvas
+        {
+            get { return displayCanvas; }
+            set { displayCanvas = value; }
+        }
+
+        private Animation[] animations;
+        public Animation[] Animations
+        {
+            get { return animations; }
+            set { animations = value; }
+        }
+
+        private Animation currentAnimation;
+        public Animation CurrentAnimation
+        {
+            get { return currentAnimation; }
+            set { currentAnimation = value; }
+        }
+
+        public void Animate()
+        {
+            //update the image source on the image component.
+        }
+    }
+}
